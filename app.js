bd64460a954026db6d48277da0bcad71fc7b8c1d
Khoa Hoang <whoskhoahoang@gmail.com>
2020-03-22 17:00:22 +0900

    Update package.json

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ var app = express();
 //====================
 //APPLICATION SETTINGS
 //====================
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 //This is for allowing cross-domain requests. all() will allow the settings, which
 //constitutes the middleware, in the function body to apply for all routes.
-app.all('*', function(req, res, next) {  
+app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -33,22 +33,22 @@ app.all('*', function(req, res, next) {
 //==========================
 //ESTABLISH ENDPOINTS/ROUTES
 //==========================
-app.get("/", function(req, res) {    
-  
+app.get("/", function(req, res) {
+
   /*
   //Testing a request within a request:
   request({uri: "http://www.example.com"}, function(error, response, body) {
-    
+
     console.log("FIRST BODY (example.com): " + body);
-    
+
     request( { uri: "http://www.google.com"}, function(error, response, body) {
-      
-     console.log("SECOND BODY (google.com): " + body); 
-      
+
+     console.log("SECOND BODY (google.com): " + body);
+
     });
   });
   */
-  
+
 });
 
 
@@ -58,10 +58,10 @@ app.get("/data/:fighter", function(req, res) {
   var first_name = name[0]; //Watch out, some fighters might have middle names too...
   var last_name = name[1];
   uri += first_name + "+" + last_name;
-  
-  console.log("Hi my name is " + first_name + " " + last_name); //FOR TESTING  
+
+  console.log("Hi my name is " + first_name + " " + last_name); //FOR TESTING
   request({ uri: uri }, function(error, response, body) {
-    if (!error) {          
+    if (!error) {
       fighter = fightFinder.parseSearchResults(body, first_name, last_name);
       fightFinder.getFighterData(fighter, res); //The server's response (res) will be prepared n the getFighterData function
     }
